424d91e080caaf6c75fcfa0cd83df12cabb86ea0
karsy <vegard.karstang@gmail.com>
2017-03-14 20:39:48 +0100

    Added saving result to png and updated gitignore.

--- mandelbrot.c
+++ mandelbrot.c
@@ -7,8 +7,11 @@
 #define limit 4
 #define size 2
 
-static void setPixel(int x, int y, int r, int g, int b) {
+static void setPixel(SDL_Surface *image, int x, int y, int r, int g, int b) {
+    Uint16 col = r << 10 + g << 5 + b;
 
+    Uint16 *pixels = (Uint16 *) image->pixels;
+    pixels[ y * image->w + x] = col;
 }
 
 static void mandelbrot(SDL_Surface *image) {
@@ -17,6 +20,8 @@ static void mandelbrot(SDL_Surface *image) {
     double real;
     double comp;
     int count = 0;
+
+
     
 }
 
@@ -39,6 +44,19 @@ int main(int argc, char *argv[]) {
         SDL_Log("IMG_Load error: %s\n", IMG_GetError());
     }
 
+    for (int i = 0; i < image->w; i++) {
+        for (int j = 0; j < image->h; j++) {
+            setPixel(image, i, j, 0b11111, 0, 0);
+        }
+    }
+
+    Uint32 pixelformat = image->format->format;
+    char *formatName = SDL_GetPixelFormatName(pixelformat);
+    printf("Format: %s\n", formatName);
+    printf("BytesPerPixel: %d\n", image->format->BytesPerPixel);
+    IMG_SavePNG(image, "out.png");
+    
+    SDL_FreeSurface(image);
     IMG_Quit();
     SDL_Quit();
 
